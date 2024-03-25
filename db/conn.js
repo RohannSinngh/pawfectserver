f26c92675f5f0f46c4cf1c73e77cca86f93ed038
qasimk02 <qassy02@gmail.com>
2024-03-25 06:03:29 +0530

    role based authentication done

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const Role = require("../model/roleSchema");
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
-
-}).then(() => {
+mongoose
+  .connect(DB, {})
+  .then(() => {
     console.log(`connnection successful`);
-}).catch((err) => console.log(`no connection`));
+    initial();
+  })
+  .catch((err) => console.log(`no connection`));
+
+async function initial() {
+  try {
+    const count = await Role.estimatedDocumentCount();
+    if (count === 0) {
+      await Promise.all([
+        new Role({ name: "user" }).save(),
+        new Role({ name: "admin" }).save(),
+      ]);
+      console.log("Roles added successfully.");
+    }
+  } catch (err) {
+    console.error("Error initializing roles:", err);
+  }
+}
